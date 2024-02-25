c070005ef6bd353a3d17e13ee0312012ec11f72c
Andrea <andrea.raneri94@gmail.com>
2024-02-25 22:23:57 +0100

    css
    sembra non funzionare bootstrap (?)

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import AllTheBooks from "../allTheBooks/AllTheBooks.jsx";
 import CommentArea from "../commentArea/CommentArea.jsx";
-import { Row, Col } from "react-bootstrap";
+import { Container,Row, Col } from "react-bootstrap";
 const Main = ()=> {
    
     return(
-        <>  
-        <Row>
-            <Col
-                className="w-75">
-                ciao
-            </Col>
-            <Col
-                className="w-25">
-                ciao2
-            </Col>
-            
-        </Row>
+        <>
+        <Container>
+            <Row style={{display:'flex'}}>
+                <Col
+                    style={{width:'70%'}}>
+                    <AllTheBooks/>
+                </Col>
+                <Col
+                    style={{width:'30%'}}>
+                    <CommentArea/>
+                </Col>
+            </Row>
+        </Container>
         </>
     )
     
